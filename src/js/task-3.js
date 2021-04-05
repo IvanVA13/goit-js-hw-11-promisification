74e8aa08d2093f3e6d384125926e5eebfea377e5
Ivan Volobuev <ivannnn2425@gmail.com>
2021-04-05 20:37:49 +0300

    доделал дз. 3

--- src/js/task-3.js
+++ src/js/task-3.js
@@ -5,10 +5,10 @@ const randomIntegerFromInterval = (min, max) => {
 const makeTransaction = (transaction) => {
   const delay = randomIntegerFromInterval(200, 500);
     return new Promise((resolve, reject) => {
-        setTimeout(() => {
-            const canProcess = Math.random() > 0.3;
+      const canProcess = Math.random() > 0.3;
+      setTimeout(() => {
             if (canProcess) {
-                resolve(transaction.id, delay);
+                resolve({id: transaction.id, time: delay});
             } else {
                reject(transaction.id);
             }
@@ -17,7 +17,7 @@ const makeTransaction = (transaction) => {
     )
 };
 
-const logSuccess = (id, time) => {
+const logSuccess = ({id, time}) => {
   console.log(`Transaction ${id} processed in ${time}ms`);
 };
 
@@ -25,16 +25,6 @@ const logError = id => {
   console.warn(`Error processing transaction ${id}. Please try again later.`);
 };
 
-/*
- * Работает так
- */
-// makeTransaction({ id: 70, amount: 150 }, logSuccess, logError);
-// makeTransaction({ id: 71, amount: 230 }, logSuccess, logError);
-// makeTransaction({ id: 72, amount: 75 }, logSuccess, logError);
-// makeTransaction({ id: 73, amount: 100 }, logSuccess, logError);
-/*
- * Должно работать так
- */
 makeTransaction({ id: 70, amount: 150 })
   .then(logSuccess)
   .catch(logError);
